f52600d6dade860b1db4d1ff6bfb3c7ed4bc204d
Chris Create <chris59create@gmail.com>
2022-10-08 07:58:49 +0200

    splice the resolved promise

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 // Parameters
 const itemsPerPage = 5; // Number of items per page of API
+const apiUri = 'http://127.0.0.1:8000/api/v1/titles/'; // Root of the API url
+// {id: {categorieName : "", genreId : x,endPoint: "",firstItem: index, numberItems: nb}}}
+const parameters = {
+    best: {categoryName: "Meilleur film", genreId: "All", endPoint: '?sort_by=-imdb_score', firstItem: 0, numberItems: 1},
+    category1: {categoryName: "Films les mieux notés", genreId:"All", endPoint: '?sort_by=-imdb_score', firstItem: 1, numberItems: 8},
+    category2: {categoryName: "Animation", genreId: 18, endPoint: "?sort_by=-imdb_score", firstItem: 0, numberItems: 7},
+    category3: {categoryName:"Mystery", genreId:9, endPoint: "?sort_by=-imdb_score", firstItem: 0, numberItems: 7},
+    category4: {categoryName: "Western", genreId: 11, endPoint: "?sort_by=-imdb_score", firstItem: 0, numberItems: 7}
+}; 
 let allRequestedMovies = [];
 let titles = []
 
 
-// Get the collection of data relative of the wished set of urls.
+// Get the collection of data relative of the wished set of urls and items number.
 async function recursiveFetch(url, numberOfItems) {
     try {
         const response = await fetch(url);
@@ -18,10 +27,11 @@ async function recursiveFetch(url, numberOfItems) {
         if (numberOfItems > 0) {
             await recursiveFetch(nextUrl, numberOfItems);
         } else {
-            console.log("Recherche terminée");
-            console.log("test response inside else..", allRequestedMovies.length);
+            console.log("Recherche terminée"); // test
+            console.log("Nombre films", allRequestedMovies.length); //test
             return allRequestedMovies;
         };
+
     } catch (err) {
         console.log(err);
         document
@@ -32,11 +42,10 @@ async function recursiveFetch(url, numberOfItems) {
 
 
 // Includes the data in the HTML
-function createListTitlelement(response) {
+function createListTitlelement(justNeededMovies, firstItem, numberItems) {
 
-    titles = response.map(movie => movie.title);
-    console.log('titles in addTitles', titles);
-
+    titles = justNeededMovies.map(movie => movie.title);
+    console.log('titles in addTitles', titles); //test
 
     const titlesElement = document.createElement("ul");
 
@@ -51,14 +60,16 @@ function createListTitlelement(response) {
 
 // main function.
 async function feedPage() {
+
     try {
-        await recursiveFetch("http://127.0.0.1:8000/api/v1/titles/?sort_by=-imdb_score", 7);
-        console.log("test response inside feedPage..", allRequestedMovies.length);
+        await recursiveFetch(apiUri+parameters.best.endPoint, parameters.best.numberItems);
+        console.log("test response inside feedPage..", allRequestedMovies.length); // test
+        console.log("nbcar deleted", allRequestedMovies.length-parameters.best.numberItems) //test
+        allRequestedMovies.splice(parameters.best.firstItem+1, allRequestedMovies.length-parameters.best.numberItems);
         createListTitlelement(allRequestedMovies);
     } catch (e) {
-        console.log("planté");
+        console.log("planté"); //test à revoir
     }
-    
 }
 
 feedPage();
