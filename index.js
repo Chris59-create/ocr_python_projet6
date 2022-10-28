890449ea5ad254e07df01c72493e49818775aa46
Chris Create <chris59create@gmail.com>
2022-10-28 10:45:06 +0200

    Put code to open modal in a single function

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Parameters
 const itemsPerPage = 5; // Number of items per page of API
 const apiUri = 'http://127.0.0.1:8000/api/v1/titles/'; // Root of the API url
+
 // {id: {categorieName : "", endPoint: "", firstItem: index, numberItems: nb}}}
 const parameters = [
     { htmlId: "best", categoryName: "En vedette", endPoint: '?sort_by=-imdb_score', firstItem: 0, numberItems: 1 },
@@ -11,24 +12,23 @@ const parameters = [
 ];
 
 // Initialize the modal and the its close button
-var modal = document.getElementById("myModal"); // Get the modal
-var span = document.getElementsByClassName("close")[0]; // Get the <span> element that closes the modal
+const modal = document.getElementById("myModal"); // Get the modal
+const span = document.getElementsByClassName("close")[0]; // Get the <span> element that closes the modal
 
-//  Create the HTML elements
+//  Create the HTML elements from the movies data collected
 async function getMoviesAllData(movies, i) {
 
+    //Select and implement the <div> relative to the treated category 
     const blocElement = document.getElementById(parameters[i].htmlId);
     
     const categoryTitle = document.createElement("h2");
     categoryTitle.innerText = parameters[i].categoryName;
-    
-
-  
+      
     const categoryElement = document.createElement("div");
     categoryElement.classList.add("scroll-category");
     categoryElement.setAttribute("id", "cat" + i);
     
-    // Create the left and right scroll buttons for the categories.
+    // Create the scroll buttons for the categories (except for the "best")
     if (i != 0) {
         const leftButton = document.createElement("button");
         leftButton.classList.add("left");
@@ -56,6 +56,7 @@ async function getMoviesAllData(movies, i) {
         blocElement.append(categoryTitle, categoryElement);
     }
    
+    // Loops in the movies ID to get with the API all needed data for a movie
     for (const index in movies) {
 
         const movieId = movies[index].id;
@@ -64,9 +65,11 @@ async function getMoviesAllData(movies, i) {
             const response = await fetch(apiUri + movieId);
             const movieData = await response.json();
 
-            // Create and append Elements for the foreground
-            // Class "formodal" helps to list the elements to pass to the modal (js not css need)
-            // Classes forBest and onlyForModal help to manage the visibility or not of elements in categories (css)
+            /* 
+            Create and append Elements for the foreground
+            Class "formodal" helps to list the elements to pass to the modal (js not css need)
+            Classes forBest and onlyForModal help to manage the visibility or not of elements in categories (css)
+            */
             
             const imageContainer = document.createElement("p");
             const imageElement = document.createElement("img");
@@ -78,7 +81,8 @@ async function getMoviesAllData(movies, i) {
 
 
             // open the modal when click on image
-            imageElement.onclick = function() {
+            imageElement.addEventListener("click", openModal);
+            function openModal() {
                 const containerElement = document.getElementById("container");
                 const newDiv = document.createElement("div")
                 newDiv.classList.add("displayModalContent")
@@ -116,30 +120,39 @@ async function getMoviesAllData(movies, i) {
             const typeElement = document.createElement("p");
             typeElement.classList.add("forModal", "onlyForModal");
             typeElement.innerText = "Genre(s): " + (movieData.genres ? movieData.genres : "" );
+
             const releaseDateElement = document.createElement("p");
             releaseDateElement.classList.add("forModal", "onlyForModal");
             releaseDateElement.innerText = "Année: " + (movieData.year ? movieData.year : "");
+
             const rateElement = document.createElement("p");
             rateElement.classList.add("forModal", "onlyForModal");
             rateElement.innerText = "Evaluation: " + (movieData.rated ? movieData.rated: "");
+
             const imdbElement = document.createElement("p");
             imdbElement.classList.add("forModal", "onlyForModal");
             imdbElement.innerText = "Imdb_score: " + (movieData.imdb_score ? movieData.imdb_score : "");
+
             const directorElement = document.createElement("p");
             directorElement.classList.add("forModal", "onlyForModal");
             directorElement.innerText = "Réalisateur(s): " + (movieData.directors ? movieData.directors : "");
+
             const actorsElement = document.createElement("p");
             actorsElement.classList.add("forModal", "onlyForModal");
             actorsElement.innerText = "Acteurs: " + (movieData.actors ? movieData.actors : "");
+
             const durationElement = document.createElement("p");
             durationElement.classList.add("forModal", "onlyForModal");
             durationElement.innerText = "Durée: " + (movieData.duration ? movieData.duration + "mn" : "");
+
             const countriesElement = document.createElement("p");
             countriesElement.classList.add("forModal", "onlyForModal");
             countriesElement.innerText = "Pays: " + (movieData.countries ? movieData.countries : "");
+
             const boxOfficeElement = document.createElement("p");
             boxOfficeElement.classList.add("forModal", "onlyForModal");
             boxOfficeElement.innerText = "Box office: " + (movieData.worldwide_gross_income ? movieData.worldwide_gross_income + "$" : "");
+
             const longDescriptionElement = document.createElement("p");
             longDescriptionElement.classList.add("forModal", "onlyForModal");
             longDescriptionElement.innerText = "Description : " + (movieData.long_description ? movieData.long_description : "");
@@ -163,7 +176,6 @@ async function getMoviesAllData(movies, i) {
             
             // List the elements to pass to the modal
             const forModal = Array.from(movieElement.getElementsByClassName("forModal"));
-            console.log(forModal);
 
             // Create the button "openModal" for the best movie block
             if (i === 0) {
@@ -172,17 +184,7 @@ async function getMoviesAllData(movies, i) {
                 btnToModal.innerText = "+ d'infos"
                 movieElement.appendChild(btnToModal);
                
-                btnToModal.onclick = function() {
-                    const containerElement = document.getElementById("container");
-                    const newDiv = document.createElement("div")
-                    newDiv.setAttribute("id", "temporary");
-                    for (let index = 0; index < forModal.length; index++) {
-                        let modalElement = forModal[index].cloneNode(true);
-                        newDiv.appendChild(modalElement);
-                    }
-                    containerElement.appendChild(newDiv);
-                    modal.style.display = "block";
-                } ;
+                btnToModal.addEventListener("click", openModal);
             }
 
             // When the user clicks on <span> (x), close the modal
@@ -209,10 +211,10 @@ async function getMoviesAllData(movies, i) {
         }
 
     }
-};
+}
 
 
-// Build the collection of data relative of the wished set of urls and items number.
+// Build with the API the collection of data relative to the wished set of urls and items number.
 async function recursiveFetch(url, allRequestedMovies, numberItems) {
 
     try {
@@ -223,6 +225,7 @@ async function recursiveFetch(url, allRequestedMovies, numberItems) {
         const nextUrl = data["next"];
         numberItems -= itemsPerPage;
 
+        // If the waited number of movies is not reached, recursive call
         if (numberItems > 0) {
             await recursiveFetch(nextUrl, allRequestedMovies, numberItems);
         } else {
@@ -265,4 +268,5 @@ async function feedPage() {
     }
 }
 
-feedPage(); // Call the main function
+// Call the main function
+feedPage();
